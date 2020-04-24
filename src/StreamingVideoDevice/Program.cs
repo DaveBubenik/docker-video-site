fb99cb25034bf09a6ba520ffae4968f777eca7f6
Dave Bubenik <dave.bubenik@deckcommerce.com>
2020-04-24 14:30:39 -0500

    Adding initial files to the repo

---
+++ src/StreamingVideoDevice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace StreamingVideoDevice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; ++i)
+            {
+                using (var vid = new Process())
+                {
+                    vid.StartInfo.FileName = "ffmpeg";
+                    vid.StartInfo.Arguments = "-re -i /Videos/sample.mp4 -c copy -f flv rtmp://wowza:1935/live/myStream";
+                    vid.StartInfo.UseShellExecute = true;
+                    vid.Start();
+                    vid.WaitForExit();
+                }
+            }
+        }
+    }
+}
